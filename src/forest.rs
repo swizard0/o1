5ab1027a123463fb327b52d94bc859f9a23bcacb
Alexey Voznyuk <me@swizard.info>
2018-07-29 03:14:28 +0300

    another global refactoring: get rid of manually written helpers like `get1`, `get2`, `get3`, etc

--- src/forest.rs
+++ src/forest.rs
@@ -16,34 +16,6 @@ impl<T> Forest1<T> {
             nodes: Set::with_capacity(capacity),
         }
     }
-
-    pub fn make_root(&mut self, item: T) -> Ref {
-        self.nodes.insert(Node { item, parent: None, })
-    }
-
-    pub fn make_node(&mut self, parent_ref: Ref, item: T) -> Ref {
-        self.nodes.insert(Node { item, parent: Some(parent_ref), })
-    }
-
-    pub fn remove(&mut self, node_ref: Ref) -> Option<T> {
-        self.nodes.remove(node_ref)
-            .map(|node| node.item)
-    }
-
-    pub fn get(&self, node_ref: Ref) -> Option<&T> {
-        self.nodes.get(node_ref)
-            .map(|node| &node.item)
-    }
-
-    pub fn get_mut(&mut self, node_ref: Ref) -> Option<&mut T> {
-        self.nodes.get_mut(node_ref)
-            .map(|node| &mut node.item)
-    }
-
-    pub fn parent(&self, node_ref: Ref) -> Option<Ref> {
-        self.nodes.get(node_ref)
-            .and_then(|node| node.parent)
-    }
 }
 
 #[derive(Clone, Copy, PartialEq, Eq, Hash, Debug)]
@@ -84,6 +56,23 @@ pub trait Forest<T> {
 
     fn access<'s, A>(&'s self, upper_layer_access: A, node_ref: Self::Ref) -> Option<(&'s T, Option<Self::Ref>)>
         where A: Fn(Self::ExternalRef) -> Option<(&'s T, Option<Self::ExternalRef>)>;
+
+    fn remove<A>(&mut self, upper_layer_access: A, node_ref: Self::Ref) -> Option<(T, Option<Self::Ref>)>
+        where A: FnMut(Self::ExternalRef) -> Option<(T, Option<Self::ExternalRef>)>;
+
+    fn get<'s, A>(&'s self, upper_layer_access: A, node_ref: Self::Ref) -> Option<&'s T>
+        where T: 's, A: Fn(Self::ExternalRef) -> Option<(&'s T, Option<Self::ExternalRef>)>
+    {
+        self.access(upper_layer_access, node_ref)
+            .map(|(item, _)| item)
+    }
+
+    fn parent<'s, A>(&'s self, upper_layer_access: A, node_ref: Self::Ref) -> Option<Self::Ref>
+        where T: 's, A: Fn(Self::ExternalRef) -> Option<(&'s T, Option<Self::ExternalRef>)>
+    {
+        self.access(upper_layer_access, node_ref)
+            .and_then(|(_, parent)| parent)
+    }
 }
 
 impl<T> Forest<T> for Forest1<T> {
@@ -103,6 +92,13 @@ impl<T> Forest<T> for Forest1<T> {
     {
         self.nodes.get(node_ref)
             .map(|node| (&node.item, node.parent))
+    }
+
+    fn remove<A>(&mut self, _upper_layer_access: A, node_ref: Self::Ref) -> Option<(T, Option<Self::Ref>)>
+        where A: FnMut(Self::ExternalRef) -> Option<(T, Option<Self::ExternalRef>)>
+    {
+        self.nodes.remove(node_ref)
+            .map(|node| (node.item, node.parent))
     }
 }
 
@@ -129,44 +125,91 @@ impl<T, F> Forest<T> for Forest2<T, F> where F: Forest<T>, F::Ref: Clone {
                     .map(|(value, parent)| (value, parent.map(Ref2::External))),
         }
     }
+
+    fn remove<A>(&mut self, mut upper_layer_access: A, node_ref: Self::Ref) -> Option<(T, Option<Self::Ref>)>
+        where A: FnMut(Self::ExternalRef) -> Option<(T, Option<Self::ExternalRef>)>
+    {
+        match node_ref {
+            Ref2::Local(local_node_ref) =>
+                self.local_nodes.remove(local_node_ref).map(|node| (node.item, node.parent.clone())),
+            Ref2::External(external_node_ref) =>
+                upper_layer_access(external_node_ref)
+                    .map(|(value, parent)| (value, parent.map(Ref2::External))),
+        }
+    }
 }
 
-pub fn get1<'a, T>(node_ref: Ref, forest: &'a Forest1<T>) -> Option<&'a T> {
-    forest.access(|()| unreachable!(), node_ref).map(|p| p.0)
+pub fn access1<'a, T, R>() -> impl Fn(R) -> Option<(&'a T, Option<R>)> {
+    move |_| unreachable!()
 }
 
-pub fn get2<'a, T>(node_ref: Ref2<Ref>, forest2: &'a Forest2<T, Forest1<T>>, forest1: &'a Forest1<T>) -> Option<&'a T> {
-    forest2.access(|node_ref| forest1.access(|()| unreachable!(), node_ref), node_ref).map(|p| p.0)
-}
-
-pub fn get3<'a, T>(
-    node_ref: Ref2<Ref2<Ref>>,
-    forest3: &'a Forest2<T, Forest2<T, Forest1<T>>>,
-    forest2: &'a Forest2<T, Forest1<T>>,
-    forest1: &'a Forest1<T>,
+pub fn access2<'a, T: 'a, F, A>(
+    forest: &'a F,
+    upper_layer_access: A,
 )
-    -> Option<&'a T>
+    -> impl Fn(F::Ref) -> Option<(&'a T, Option<F::Ref>)>
+    where F: Forest<T>,
+          A: Fn(F::ExternalRef) -> Option<(&'a T, Option<F::ExternalRef>)>,
 {
-    forest3.access(
-        |node_ref| forest2.access(|node_ref| forest1.access(|()| unreachable!(), node_ref), node_ref),
-        node_ref,
-    ).map(|p| p.0)
+    move |node_ref| forest.access(&upper_layer_access, node_ref)
 }
 
-pub fn get4<'a, T>(
-    node_ref: Ref2<Ref2<Ref2<Ref>>>,
-    forest4: &'a Forest2<T, Forest2<T, Forest2<T, Forest1<T>>>>,
-    forest3: &'a Forest2<T, Forest2<T, Forest1<T>>>,
-    forest2: &'a Forest2<T, Forest1<T>>,
-    forest1: &'a Forest1<T>,
+pub struct TowardsRootIter<R, A> {
+    cursor: Option<R>,
+    layer_access: A,
+}
+
+pub fn towards_root_iter<'a, T: 'a, R, A, F>(
+    forest1: &'a F,
+    upper_layer_access: A,
+    node_ref: R,
 )
-    -> Option<&'a T>
+    -> TowardsRootIter<R, impl Fn(R) -> Option<(&'a T, Option<R>)>>
+    where F: Forest<T, Ref = R>,
+          A: Fn(F::ExternalRef) -> Option<(&'a T, Option<F::ExternalRef>)>,
 {
-    forest4.access(
-        |node_ref| forest3.access(
-            |node_ref| forest2.access(|node_ref| forest1.access(|()| unreachable!(), node_ref), node_ref),
-            node_ref,
-         ),
-        node_ref,
-    ).map(|p| p.0)
+    TowardsRootIter {
+        cursor: Some(node_ref),
+        layer_access: move |node_ref| forest1.access(&upper_layer_access, node_ref),
+    }
+}
+
+impl<'a, T: 'a, R, A> Iterator for TowardsRootIter<R, A> where R: Clone, A: Fn(R) -> Option<(&'a T, Option<R>)> {
+    type Item = (R, &'a T);
+
+    fn next(&mut self) -> Option<Self::Item> {
+        let node_ref = self.cursor.take()?;
+        let (item, parent) = (self.layer_access)(node_ref.clone())?;
+        self.cursor = parent;
+        Some((node_ref, item))
+    }
+}
+
+
+#[cfg(test)]
+mod test {
+    use super::{
+        Forest,
+        Forest1,
+        access1,
+        towards_root_iter,
+    };
+
+    #[test]
+    fn forest1() {
+        let mut forest1 = Forest1::new();
+        let root = forest1.make_root("root");
+        let child_a = forest1.make_node(root, "child_a");
+        assert_eq!(forest1.parent(access1(), child_a), Some(root));
+        let child_b = forest1.make_node(root, "child_b");
+        let child_c = forest1.make_node(child_a, "child_c");
+
+        assert_eq!(forest1.get(access1(), root), Some(&"root"));
+        assert_eq!(forest1.get(access1(), child_a), Some(&"child_a"));
+        assert_eq!(forest1.get(access1(), child_b), Some(&"child_b"));
+        assert_eq!(forest1.get(access1(), child_c), Some(&"child_c"));
+
+        let rev_path: Vec<_> = towards_root_iter(&forest1, access1(), child_c).map(|p| p.1).collect();
+        assert_eq!(rev_path, vec![&"child_c", &"child_a", &"root"]);
+    }
 }
